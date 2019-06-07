00e61fad3c57487d50ae36250212591e2d665d41
david <noah.robert45@outlook.fr>
2019-06-07 18:02:52 -0400

    first big commit

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #path('accueil', views.home),
+    #path('article/<id_article>', views.view_article),
+    #path('date', views.date_actuelle),
+    #path('addition/<int:nombre1>/<int:nombre2>', views.addition),
+    path('', views.accueil, name='accueil'),
+    path('article/<int:id>', views.lire, name='lire'),
+    path('contact/', views.contact, name='contact'),
+    path('test/', views.test_article, name='test_article'),
+#    path('article/<int:id>-<slug:slug>', views.lire, name='lire')
+]
---
+++ blog/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from datetime import datetime
+from django.shortcuts import render
+
+def home(request):
+    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+    return HttpResponse("""
+        <h1>Bienvenue sur mon blog !</h1>
+        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
+    """)
+
+# Create your views here.
+def view_article(request, id_article,slug):
+    """ 
+    Vue qui affiche un article selon son identifiant (ou ID, ici un numéro)
+    Son ID est le second paramètre de la fonction (pour rappel, le premier
+    paramètre est TOUJOURS la requête de l'utilisateur)
+    """
+    return HttpResponse(
+        "Vous avez demandé l'article n° {0} !".format(id_article)    
+    )
+
+def date_actuelle(request):
+    return render(request, 'blog/date.html', {'date': datetime.now()})
+
+
+def addition(request, nombre1, nombre2):    
+    total = nombre1 + nombre2
+
+    # Retourne nombre1, nombre2 et la somme des deux au tpl
+    return render(request, 'blog/addition.html', locals())
+
+
+from blog.models import Article
+
+def accueil(request):
+    """ Afficher tous les articles de notre blog """
+    articles = Article.objects.all() # Nous sélectionnons tous nos articles
+    return render(request, 'blog/accueil.html', {'derniers_articles': articles})
+
+# Il faut ajouter l'import get_object_or_404, attention !
+from django.shortcuts import render, get_object_or_404
+
+def lire(request, id):
+    article = get_object_or_404(Article, id=id)
+    return render(request, "blog/lire.html", {'article' : article})
+
+
+from .forms import ContactForm
+
+
+def contact(request):
+    # Construire le formulaire, soit avec les données postées,
+    # soit vide si l'utilisateur accède pour la première fois
+    # à la page.
+    form = ContactForm(request.POST or None)
+    # Nous vérifions que les données envoyées sont valides
+    # Cette méthode renvoie False s'il n'y a pas de données
+    # dans le formulaire ou qu'il contient des erreurs.
+    if form.is_valid():
+        # Ici nous pouvons traiter les données du formulaire
+        sujet = form.cleaned_data['sujet']
+        message = form.cleaned_data['message']
+        envoyeur = form.cleaned_data['envoyeur']
+        renvoi = form.cleaned_data['renvoi']
+
+        # Nous pourrions ici envoyer l'e-mail grâce aux données
+        # que nous venons de récupérer
+        envoi = True
+
+    # Quoiqu'il arrive, on affiche la page du formulaire.
+    return render(request, 'blog/contact.html', locals())
+
+from .forms import ArticleForm
+def test_article(request):
+    # Construire le formulaire, soit avec les données postées,
+    # soit vide si l'utilisateur accède pour la première fois
+    # à la page.
+    form = ArticleForm(request.POST or None)
+    # Nous vérifions que les données envoyées sont valides
+    # Cette méthode renvoie False s'il n'y a pas de données
+    # dans le formulaire ou qu'il contient des erreurs.
+    if form.is_valid():
+        pass
+    # Quoiqu'il arrive, on affiche la page du formulaire.
+    return render(request, 'blog/contact.html', locals())
