72990819684ed6b0d05136b7a32830f61199579b
david <noah.robert45@outlook.fr>
2019-06-17 17:44:25 -0400

    Les signaux et middlewares

---
+++ toto/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Eleve(models.Model):
+    nom = models.CharField(max_length=31)
+    moyenne = models.IntegerField(default=10)
+
+    def __str__(self):
+        return "Élève {0} ({1}/20 de moyenne)".format(self.nom, self.moyenne)
+
+class Cours(models.Model):
+    nom = models.CharField(max_length=31)
+    eleves = models.ManyToManyField(Eleve)
+
+    def __str__(self):
+        return self.nom
